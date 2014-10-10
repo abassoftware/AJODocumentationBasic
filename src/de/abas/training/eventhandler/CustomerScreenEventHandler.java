1aed2da97d32009326a9db29c3a26a1165a6bed1
Jasmin Scheffel <jasc@abas.de>
2014-10-10 11:56:56 +0200

    CustomerScreenEventHandler: ScreenValidation event added

--- src/de/abas/training/eventhandler/CustomerScreenEventHandler.java
+++ src/de/abas/training/eventhandler/CustomerScreenEventHandler.java
@@ -1,14 +1,19 @@
 package de.abas.training.eventhandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.abas.erp.axi.event.EventException;
 import de.abas.erp.axi.screen.ScreenControl;
 import de.abas.erp.axi2.EventHandlerRunner;
 import de.abas.erp.axi2.annotation.EventHandler;
 import de.abas.erp.axi2.annotation.ScreenEventHandler;
+import de.abas.erp.axi2.event.ScreenEndEvent;
 import de.abas.erp.axi2.event.ScreenEvent;
 import de.abas.erp.axi2.type.ScreenEventType;
 import de.abas.erp.common.type.enums.EnumEditorAction;
 import de.abas.erp.db.DbContext;
+import de.abas.erp.db.field.TypedField;
 import de.abas.erp.db.schema.company.Password;
 import de.abas.erp.db.schema.customer.CustomerEditor;
 import de.abas.erp.db.schema.employee.Employee;
@@ -16,6 +21,7 @@ import de.abas.erp.db.selection.ExpertSelection;
 import de.abas.erp.db.selection.Selection;
 import de.abas.erp.db.util.QueryUtil;
 import de.abas.erp.jfop.rt.api.annotation.RunFopWith;
+import de.abas.jfop.base.Color;
 import de.abas.jfop.base.buffer.GlobalTextBuffer;
 import de.abas.jfop.base.buffer.internal.BufferFactoryImpl;
 
@@ -61,6 +67,112 @@ public class CustomerScreenEventHandler {
 				}
 			}
 		}
+	}
+
+	@ScreenEventHandler(type = ScreenEventType.CANCEL)
+	public void screenCancel(ScreenEvent event, ScreenControl screenControl,
+			DbContext ctx, CustomerEditor head) throws EventException {
+		// TODO Auto-generated method stub
+	}
+
+	@ScreenEventHandler(type = ScreenEventType.END)
+	public void screenEnd(ScreenEndEvent event, ScreenControl screenControl,
+			DbContext ctx, CustomerEditor head) throws EventException {
+		// TODO Auto-generated method stub
+	}
+
+	@ScreenEventHandler(type = ScreenEventType.EXIT)
+	public void screenExit(ScreenEvent event, ScreenControl screenControl,
+			DbContext ctx, CustomerEditor head) throws EventException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Checks whether all mandatory fields are filled.
+	 * 
+	 * @param event The event that occurred.
+	 * @param screenControl The ScreenControl instance.
+	 * @param ctx The database context.
+	 * @param head The CustomerEditor instance.
+	 * @throws EventException The exception thrown if not all mandatory fields are filled.
+	 */
+	@ScreenEventHandler(type = ScreenEventType.VALIDATION)
+	public void screenValidation(ScreenEvent event,
+			ScreenControl screenControl, DbContext ctx, CustomerEditor head)
+			throws EventException {
+		List<TypedField<CustomerEditor>> fields = new ArrayList<TypedField<CustomerEditor>>();
+		fields = markMandatoryFields(screenControl, head, fields);
+		if(!fields.isEmpty()) {
+			screenControl.moveCursor(head, fields.get(0));
+			String listOfFields = getFieldNames(fields);
+			throw new EventException("The following fields are empty but mandatory:\n" + listOfFields);
+		}
+	}
+
+	/**
+	 * Stores the names of the fields stored in the List in a String separating them with a line break.
+	 * 
+	 * @param fields The list of fields.
+	 * @return The string containing the names of the fields.
+	 */
+	private String getFieldNames(List<TypedField<CustomerEditor>> fields) {
+		String listOfFields = "";
+		for (TypedField<CustomerEditor> field : fields) {
+			listOfFields += field.getName() + "\n";
+		}
+		return listOfFields;
+	}
+
+	/**
+	 * Checks whether to mark the mandatory fields.
+	 * 
+	 * @param screenControl The ScreenControl instance.
+	 * @param head The CustomerEditor instance.
+	 * @param fields The empty list to story unfilled mandatory fields.
+	 * @return The list containing all unfilled mandatory fields.
+	 */
+	private List<TypedField<CustomerEditor>> markMandatoryFields(ScreenControl screenControl,
+			CustomerEditor head, List<TypedField<CustomerEditor>> fields) {
+		fields = markMandatoryField(screenControl, head, head.getDescrOperLang().isEmpty(),
+				CustomerEditor.META.descrOperLang, fields);
+		fields = markMandatoryField(screenControl, head, head.getAddr().isEmpty(),
+				CustomerEditor.META.addr, fields);
+		fields = markMandatoryField(screenControl, head, head.getStreet().isEmpty(),
+				CustomerEditor.META.street, fields);
+		fields = markMandatoryField(screenControl, head, head.getZipCode().isEmpty(),
+				CustomerEditor.META.zipCode, fields);
+		fields = markMandatoryField(screenControl, head, head.getTown().isEmpty(),
+				CustomerEditor.META.town, fields);
+		fields = markMandatoryField(screenControl, head, head.getStateOfTaxOffice() == null,
+				CustomerEditor.META.stateOfTaxOffice, fields);
+		fields = markMandatoryField(screenControl, head, head.getContactPerson().isEmpty(),
+				CustomerEditor.META.contactPerson, fields);
+		fields = markMandatoryField(screenControl, head, head.getInhouseContact() == null,
+				CustomerEditor.META.inhouseContact, fields);
+		fields = markMandatoryField(screenControl, head, head.getPayTerm() == null,
+				CustomerEditor.META.payTerm, fields);
+		return fields;
+	}
+
+	/**
+	 * Colors the mandatory field if empty.
+	 * 
+	 * @param screenControl The ScreenControl instance.
+	 * @param head The CustomerEditor instance.
+	 * @param isEmpty Whether or not the field is empty.
+	 * @param field The field.
+	 * @param fields The list to add the field to if it is empty.
+	 * @return The list of empty fields.
+	 */
+	private List<TypedField<CustomerEditor>> markMandatoryField(ScreenControl screenControl,
+			CustomerEditor head, boolean isEmpty,
+			TypedField<CustomerEditor> field, List<TypedField<CustomerEditor>> fields) {
+		screenControl.setColor(head, field, Color.DEFAULT, Color.DEFAULT);
+		if (isEmpty) {
+			screenControl.setColor(head, field, Color.DEFAULT, Color.RED);
+			fields.add(field);
+		}
+		return fields;
 	}
 
 }
