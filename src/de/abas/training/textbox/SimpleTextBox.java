04f86f3e7545d4a02194910412702ab7ebb2c331
Jasmin Scheffel <jasc@abas.de>
2014-11-19 08:57:50 +0100

    Example for creating and showing a simple text box

---
+++ src/de/abas/training/textbox/SimpleTextBox.java
@@ -0,0 +1,23 @@
+package de.abas.training.textbox;
+
+import de.abas.erp.api.gui.TextBox;
+import de.abas.training.common.AbstractAjoAccess;
+
+/**
+ * Shows how to display a simple text box.
+ *
+ * @author abas Software AG
+ *
+ */
+public class SimpleTextBox extends AbstractAjoAccess {
+
+	@Override
+	public void run(String[] args) {
+		final TextBox textBox =
+				new TextBox(getDbContext(), "Title of the TextBox",
+						"Content of the TextBox");
+		textBox.show();
+
+	}
+
+}
