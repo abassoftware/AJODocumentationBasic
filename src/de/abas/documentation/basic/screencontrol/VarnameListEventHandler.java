caad4b5934e7827418758d01e86d362b70f6949d
Jasmin Scheffel <jasc@abas.de>
2015-02-03 14:57:01 +0100

    example for screen protection added

---
+++ src/de/abas/documentation/basic/screencontrol/VarnameListEventHandler.java
@@ -0,0 +1,40 @@
+package de.abas.documentation.basic.screencontrol;
+
+import de.abas.erp.axi.event.EventException;
+import de.abas.erp.axi.screen.ScreenControl;
+import de.abas.erp.axi2.EventHandlerRunner;
+import de.abas.erp.axi2.annotation.EventHandler;
+import de.abas.erp.axi2.annotation.ScreenEventHandler;
+import de.abas.erp.axi2.event.ScreenEvent;
+import de.abas.erp.axi2.type.ScreenEventType;
+import de.abas.erp.db.DbContext;
+import de.abas.erp.db.infosystem.custom.ow1.InfosystemVARNAMELIST;
+import de.abas.erp.jfop.rt.api.annotation.RunFopWith;
+import de.abas.jfop.base.buffer.BufferFactory;
+import de.abas.jfop.base.buffer.GlobalTextBuffer;
+
+@EventHandler(head = InfosystemVARNAMELIST.class,
+		row = InfosystemVARNAMELIST.Row.class)
+@RunFopWith(EventHandlerRunner.class)
+public class VarnameListEventHandler {
+
+	@ScreenEventHandler(type = ScreenEventType.ENTER)
+	public void screenEnter(ScreenEvent event, ScreenControl screenControl,
+			DbContext ctx, InfosystemVARNAMELIST head) throws EventException {
+
+		// access to global variable table (V-13-00)
+		BufferFactory bufferFactory = BufferFactory.newInstance();
+		GlobalTextBuffer globalTextBuffer = bufferFactory.getGlobalTextBuffer();
+		String operatorID = globalTextBuffer.getStringValue("operatorCode");
+
+		// protect start button if users do not have operator ID CK, CH, TK or ABAS
+		if (operatorID.equals("CK") || operatorID.equals("CH")
+				|| operatorID.equals("TK") || operatorID.equals("ABAS")) {
+
+		}
+		else {
+			screenControl
+					.setProtection(head, InfosystemVARNAMELIST.META.start, true);
+		}
+	}
+}
